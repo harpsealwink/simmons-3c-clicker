a2dbd58b1fd720320aeb8b2cdbd0b09895f7c31e
Grace Jiang <gracejiang@dhcp-10-31-91-80.dyn.mit.edu>
2022-01-19 22:34:39 -0500

    deployment debugging

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ReactHover, { Trigger, Hover } from 'react-hover'
 
 import './App.css';
--- src/Popup.js
+++ src/Popup.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './Popup.css';
 import './App.css';
 
